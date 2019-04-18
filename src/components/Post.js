971c5c4b7dba7b04fc5a3c7ea224a85a3c2432f7
Mauricio Blum <mauriciocblum@gmail.com>
2019-04-18 20:53:10 -0300

    Added Post and Index Layout and Logic

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,13 +1,57 @@
-import React, { Component } from "react";
+import React from 'react';
 
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text } from 'react-native';
 
-export default class components extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Post</Text>
-      </View>
-    );
-  }
-}
+import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create({
+  postContainer: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 5,
+    marginTop: 20,
+    marginHorizontal: 20,
+  },
+  postHeader: {
+    textAlign: 'left',
+    borderBottomWidth: 1,
+
+    borderColor: '#999',
+  },
+  postTitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#222',
+  },
+  postAuthor: {
+    fontSize: 12,
+    fontWeight: '300',
+    color: '#666',
+  },
+  postDescription: {
+    fontSize: 14,
+    fontWeight: 'normal',
+    textAlign: 'left',
+    color: '#666',
+    marginVertical: 5,
+  },
+});
+
+const Post = ({ title, author, description }) => (
+  <View style={styles.postContainer}>
+    <View style={styles.postHeader}>
+      <Text style={styles.postTitle}>{title}</Text>
+      <Text style={styles.postAuthor}>{author}</Text>
+    </View>
+
+    <Text style={styles.postDescription}>{description}</Text>
+  </View>
+);
+
+Post.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default Post;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,60 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from 'react';
+import {
+  StyleSheet, Text, View, ScrollView,
+} from 'react-native';
 
-import Post from "~/components/Post";
+import Post from '~/components/Post';
 
 export default class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 0,
+        title: 'Aprendendo React Native',
+        author: 'Mauricio Blum',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac posuere nulla. Nullam vitae ipsum dictum, placerat libero eget, lobortis velit. Vivamus quis faucibus justo. Aliquam erat volutpat. Mauris volutpat placerat nibh, eu sodales urna commodo id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur vitae lacus ut dolor pharetra malesuada. Nullam pharetra ut diam scelerisque sagittis. Suspendisse potenti. Maecenas dignissim, justo viverra ultrices pulvinar, dolor nunc vestibulum libero, quis commodo sem quam a leo.        ',
+      },
+      {
+        id: 1,
+        title: 'Aprendendo React',
+        author: 'Mauricio Blum',
+        description: 'Lorem Ipsum Dolor Sit Amet',
+      },
+      {
+        id: 2,
+        title: 'Aprendendo Node',
+        author: 'Mauricio Blum',
+        description: 'Lorem Ipsum Dolor Sit Amet',
+      },
+      {
+        id: 3,
+        title: 'Aprendendo a Jogar',
+        author: 'Mauricio Blum',
+        description: 'Lorem Ipsum Dolor Sit Amet',
+      },
+      {
+        id: 4,
+        title: 'Aprendendo a Ganhar',
+        author: 'Mauricio Blum',
+        description: 'Lorem Ipsum Dolor Sit Amet',
+      },
+    ],
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Hello World</Text>
-        <Post />
+        <ScrollView style={styles.scroll}>
+          {this.state.posts.map(post => (
+            <Post
+              key={post.id}
+              title={post.title}
+              author={post.author}
+              description={post.description}
+            />
+          ))}
+        </ScrollView>
       </View>
     );
   }
@@ -16,9 +62,12 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  }
+    flex: 0,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#ee7777',
+  },
+  scroll: {
+    width: '100%',
+  },
 });
